ff7d82285d3d3806f8d00dfb3b838e56b97cf843
Mack <macknz7@gmail.com>
2021-08-18 20:42:31 -0400

    weapons, fighter, damage

---
+++ Weapon.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : Collidable
+{
+    // Damage struct
+    public int damagePoint = 1;
+    public float pushForce = 2.0f;
+
+    // Upgrade
+    public int weaponLevel = 0;
+    private SpriteRenderer spriteRenderer;
+
+    // Swing
+    private float cooldown = 0.5f;
+    private float lastSwing;
+
+    protected override void Start() 
+    {
+        base.Start();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(Time.time - lastSwing > cooldown)
+            {
+                lastSwing = Time.time;
+                Swing();
+            }
+        }
+    }
+
+    private void Swing()
+    {
+        Debug.Log("Swing");
+    }
+
+    protected override void OnCollilde(Collider2D coll)
+    {
+        if(coll.tag == "Fighter")
+        {
+            if(coll.name != "Player")
+            {
+                Debug.Log(coll.name);            
+                var dmg = new Damage{
+                    damageAmount = damagePoint,
+                    origin = transform.position,
+                    pushForce = pushForce
+                };
+
+                coll.SendMessage("ReceivedDamage", dmg);
+            }
+        }
+    }
+}
